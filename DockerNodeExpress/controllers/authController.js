bfdf9fc00e7f43dcb0491627db5706adfb1f0be3
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2022-09-19 21:25:34 -0700

    Checkpoint: Add Redis

--- DockerNodeExpress/controllers/authController.js
+++ DockerNodeExpress/controllers/authController.js
@@ -1,14 +1,53 @@
 const User = require("../models/userModal");
+const bcrypt = require("bcryptjs");
 
 exports.signUp = async (req, res) => {
+  const { username, password } = req.body;
+
   try {
-    const newUser = await User.create(req.body);
+    const hashpassword = await bcrypt.hash(password, 12);
+    const newUser = await User.create({
+      username,
+      password: hashpassword,
+    });
     res.status(201).json({
       status: "success",
       data: {
         user: newUser,
       },
     });
+  } catch (e) {
+    res.status(400).json({
+      status: "fail",
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({
+        status: "fail",
+        message: "user not found",
+      });
+    }
+
+    const isCorrect = await bcrypt.compare(password, user.password);
+
+    if (isCorrect) {
+      res.status(200).json({
+        status: "success",
+      });
+    } else {
+      res.status(400).json({
+        status: "fail",
+        message: "Incorrect username or password",
+      });
+    }
   } catch (e) {
     res.status(400).json({
       status: "fail",
